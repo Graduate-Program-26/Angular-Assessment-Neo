987ae799b56a9562f996bc38a2bcdefdf6fe9a53
Your Name <NRankapole@dvtsoftware.com>
2026-05-03 19:49:42 +0200

    feat: Define basic routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { Album } from './features/album/album';
+import { Artist } from './features/artist/artist';
+import { Playlist } from './features/playlist/playlist';
+import { Home } from './features/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: Home,
+    },
+    {
+        path: 'album',
+        component: Album,
+    },
+    {
+        path: 'artist',
+        component: Artist,
+    },
+    {
+        path: 'playlist',
+        component: Playlist,
+    },
+];
