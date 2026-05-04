589384bf7f582bda8ebeedfdd689213cae89357d
Your Name <NRankapole@dvtsoftware.com>
2026-05-04 14:21:04 +0200

    feat: Create playlist-list and user-playlist components

---
+++ src/app/features/playlist/playlist-list/playlist-list.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-playlist-list',
+  templateUrl: './playlist-list.html',
+  styleUrl: './playlist-list.css',
+})
+export class PlaylistList {}
---
+++ src/app/features/playlist/user-playlist/user-playlist.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-playlist',
+  templateUrl: './user-playlist.html',
+  styleUrl: './user-playlist.css',
+})
+export class UserPlaylist {
+}
