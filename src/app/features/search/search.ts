e4f5c4e088d39e6d98d1abcb35f64d3ce325fad3
Your Name <NRankapole@dvtsoftware.com>
2026-05-04 12:19:17 +0200

    Feat: Implement search with debounce in search component

--- src/app/features/search/search.ts
+++ src/app/features/search/search.ts
@@ -1,9 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { debounceTime, distinctUntilChanged, filter, switchMap } from 'rxjs';
+import { DeezerService } from '../../shared/services/deezer.service';
 
 @Component({
   selector: 'app-search',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './search.html',
   styleUrl: './search.css',
 })
-export class Search {}
+export class Search {
+  private deezerService = inject(DeezerService);
+
+  query = new FormControl<string>('', { nonNullable: true });
+
+  results = toSignal(
+    this.query.valueChanges.pipe(
+      debounceTime(450),
+      distinctUntilChanged(),
+      filter(query => query.trim().length > 0),
+      switchMap(query => this.deezerService.search(query))
+    )
+  );
+}
