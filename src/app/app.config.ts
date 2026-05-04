c50db23ca0c0aab25061ff579090e57400d5a202
Your Name <NRankapole@dvtsoftware.com>
2026-05-04 20:36:37 +0200

    feat: Remove NgZorro configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,19 +1,12 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
-
 import { routes } from './app.routes';
-import { en_US, provideNzI18n } from 'ng-zorro-antd/i18n';
-import { registerLocaleData } from '@angular/common';
-import en from '@angular/common/locales/en';
-
-registerLocaleData(en);
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes, withComponentInputBinding()),
-    provideNzI18n(en_US),
     provideHttpClient(),
   ],
 };
