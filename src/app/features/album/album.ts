c7d86829cbfc54d37862ff4b419e7d94b195670a
Your Name <NRankapole@dvtsoftware.com>
2026-05-05 14:38:28 +0200

    feat: Display selected album's information

--- src/app/features/album/album.ts
+++ src/app/features/album/album.ts
@@ -1,11 +1,14 @@
 import { Component, inject, input } from '@angular/core';
 import { toSignal, toObservable } from '@angular/core/rxjs-interop';
-import { switchMap } from 'rxjs';
+import { filter, switchMap } from 'rxjs';
+import { TitleCasePipe } from '@angular/common';
+import { RouterLink } from '@angular/router';
 import { DeezerService } from '../../shared/services/deezer.service';
+import { DurationPipe } from '../../shared/pipes/duration.pipe';
 
 @Component({
   selector: 'app-album',
-  imports: [],
+  imports: [DurationPipe, RouterLink, TitleCasePipe],
   templateUrl: './album.html',
   styleUrl: './album.css',
 })
@@ -13,7 +16,12 @@ export class Album {
   private deezerService = inject(DeezerService);
 
   id = input.required<string>();
+
+  private id$ = toObservable(this.id).pipe(
+    filter(id => !!id && !isNaN(Number(id)))
+  );
+
   album = toSignal(
-    toObservable(this.id).pipe(switchMap((id) => this.deezerService.getAlbum(Number(id)))),
+    this.id$.pipe(switchMap(id => this.deezerService.getAlbum(Number(id))))
   );
 }
