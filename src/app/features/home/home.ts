32330d13e761b4fbbbc08c66e98e35283dc25c94
Your Name <NRankapole@dvtsoftware.com>
2026-05-06 21:46:23 +0200

    feat: Display chart information on home page

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,10 +1,13 @@
 import { Component, computed, inject } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { DeezerService } from '../../shared/services/deezer.service';
+import { ArtistCard } from '../../shared/components/artist-card/artist-card';
+import { AlbumCard } from '../../shared/components/album-card/album-card';
+import { TrackItem } from '../../shared/components/track/track';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [ArtistCard, AlbumCard, TrackItem],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
