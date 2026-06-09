c7e3fda18ecfb4a8f79af848349cb791b1027482
Your Name <NRankapole@dvtsoftware.com>
2026-06-09 21:11:39 +0200

    fix: restore environment.ts and untrack from .gitignore

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAeQL7RoS3UYsMQ_X_Prr-FM0pEqYXz7jg",
+    authDomain: "mydeezer-app.firebaseapp.com",
+    projectId: "mydeezer-app",
+    storageBucket: "mydeezer-app.firebasestorage.app",
+    messagingSenderId: "686058437750",
+    appId: "1:686058437750:web:c2c8f4fb9dd4a3c302f357",
+    measurementId: "G-TR9CCMKFF7"
+  },
+};
